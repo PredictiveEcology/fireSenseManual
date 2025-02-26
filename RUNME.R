a486558572beea4868194f9073eb9fd94713cdcd
Ian Eddy <ianmseddy@gmail.com>
2025-02-25 16:41:29 -0800

    updates

--- RUNME.R
+++ RUNME.R
@@ -1,33 +1,18 @@
 
 ## this manual must be knitted by running this script
 
-prjDir <- SpaDES.project::findProjectPath()
+#fix this
+prjPath <- projectPath
 
-Require::Require(c("yaml", "purrr", "fs"))
 
 docsDir <- file.path(prjDir, "_bookdown.yml") |>
   yaml::read_yaml() |>
   purrr::pluck("output_dir") |>
   fs::path_abs()
 
-pkgPath <- normalizePath(file.path("packages", version$platform,
-                                   paste0(version$major, ".", strsplit(version$minor, "[.]")[[1]][1])),
-                         winslash = "/")
-dir.create(pkgPath, recursive = TRUE)
-.libPaths(pkgPath, include.site = FALSE)
+bibDir <- Require::checkPath(file.path(prjDir, "citations"), create = TRUE)
+figDir <- Require::checkPath(file.path(docsDir, "figures"), create = TRUE)
 
-Require::Require(c("bookdown", "ROpenSci/bibtex", "data.table", "downlit",
-                   "formatR", "git2r", "kableExtra", "yihui/knitr", 
-                   "fansi", "xml2", "vctrs", "RefManageR", "remotes", "git2r"))
-remotes::install_github("PredictiveEcology/SpaDES.docs@development")
-
-
-# bibDir <- Require::checkPath(file.path(prjDir, "citations"), create = TRUE)
-# figDir <- Require::checkPath(file.path(docsDir, "figures"), create = TRUE)
-
-options(
-  Ncpus = min(parallel::detectCores() / 2, 8)
-)
 
 # load packages -------------------------------------
 
@@ -41,31 +26,31 @@ library(formatR)
 
 ## references ---------------------------------------
 
-## automatically create a bib database for R packages
-# allPkgs <- .packages(all.available = TRUE, lib.loc = .libPaths()[1])
-# keyPkgs <- c(
-#   "bookdown", "knitr", "LandR", "LandWebUtils",
-#   "reproducible", "rmarkdown", "shiny", "SpaDES.core", "SpaDES.tools"
-# )
-# write_bib(allPkgs, file.path(bibDir, "packages.bib")) ## TODO: using allPkgs, not all pkgs have dates/years
-# 
-# ## collapse all chapter .bib files into one ------
-# bibFiles <- c(
-#   list.files(file.path(prjDir, "m"), "references_", recursive = TRUE, full.names = TRUE),
-#   file.path(bibDir, "packages.bib"),
-#   file.path(bibDir, "references.bib")
-# )
-# bibdata <- lapply(bibFiles, function(f) {
-#   if (file.exists(f)) RefManageR::ReadBib(f)
-# })
-# bibdata <- Reduce(merge, bibdata)
-# 
-# WriteBib(bibdata, file = file.path(bibDir, "references.bib"))
-# 
-# csl <- file.path(bibDir, "ecology-letters.csl")
-# if (!file.exists(csl)) {
-#   download.file("https://www.zotero.org/styles/ecology-letters?source=1", destfile = csl)
-# }
+# automatically create a bib database for R packages
+allPkgs <- .packages(all.available = TRUE, lib.loc = .libPaths()[1])
+keyPkgs <- c(
+  "bookdown", "knitr", "LandR", "fireSenseUtils",
+  "reproducible", "rmarkdown", "shiny", "SpaDES.core", "SpaDES.tools"
+)
+write_bib(allPkgs, file.path(bibDir, "packages.bib")) ## TODO: using allPkgs, not all pkgs have dates/years
+
+## collapse all chapter .bib files into one ------
+bibFiles <- c(
+  list.files(file.path(prjDir, "m"), "references_", recursive = TRUE, full.names = TRUE),
+  file.path(bibDir, "packages.bib"),
+  file.path(bibDir, "references.bib")
+)
+bibdata <- lapply(bibFiles, function(f) {
+  if (file.exists(f)) RefManageR::ReadBib(f)
+})
+bibdata <- Reduce(merge, bibdata)
+
+WriteBib(bibdata, file = file.path(bibDir, "references.bib"))
+
+csl <- file.path(bibDir, "ecology-letters.csl")
+if (!file.exists(csl)) {
+  download.file("https://www.zotero.org/styles/ecology-letters?source=1", destfile = csl)
+}
 
 ## RENDER BOOK ------------------------------------------
 
@@ -78,6 +63,7 @@ if (!file.exists(file.path(prjDir, ".nojekyll"))) {
 
 ## set manual version
 Sys.setenv(FIRESENSE_MAN_VERSION = "0.1") ## update this for each new release
+# look up how to do this automatically 
 Sys.getenv("R_USE_REQUIRE")
 
 ## NOTE: need dot because knitting is doing `rm(list = ls())`
