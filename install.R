ca8b825a3435e556fb3f457bf15dc9af7962cc94
Alex Chubaty <achubaty@for-cast.ca>
2025-04-08 22:45:26 -0600

    use install.R in GHA workflow to ensure consistency

--- install.R
+++ install.R
@@ -1,10 +1,8 @@
-if (interactive()) {
-  ## install packages from project DESCRIPTION
-  remotes::install_deps()
-  
-  ## install additional packages used by modules
-  SpaDES.core::packages(modules = list.files("modules"), paths = "modules") |>
-    unlist() |>
-    Require::pkgDep2() |>
-    Require::Install()
-}
+## install packages from project DESCRIPTION
+remotes::install_deps()
+
+## install additional packages used by modules
+SpaDES.core::packages(modules = list.files("modules"), paths = "modules") |>
+  unlist() |>
+  Require::pkgDep2() |>
+  Require::Install()
